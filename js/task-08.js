06c494f608819e9b41fb16cb60468f06a4223589
Helarios <kuzinaksenya32@gmail.com>
2021-12-23 19:41:20 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const { email, password } = formRef.elements;
+
+    if (email.value === '' || password.value === '') {
+        alert('Заполните все поля!');
+        return;
+    }
+
+    const info = {
+        email: email.value,
+        password: password.value,
+    };
+    console.log(info);
+
+    formRef.reset();
+});
