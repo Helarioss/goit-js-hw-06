edce7b1fb318646cef07ddf9a854da4b54af271d
Helarios <kuzinaksenya32@gmail.com>
2021-12-23 20:10:10 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
+const numberRef = document.querySelector('#controls > input');
+const createBtnRef = document.querySelector('[data-create]');
+const destroyBtnRef = document.querySelector('[data-destroy]');
+const containerBoxesRef = document.querySelector('#boxes');
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+    const boxes = [];
+    let size = 30;
+    for (let i = 1; i <= amount; i += 1) {
+        const box = document.createElement('div');
+        box.style.width = size + 'px';
+        box.style.height = size + 'px';
+        box.style.backgroundColor = getRandomHexColor();
+
+        boxes.push(box);
+
+        size += 10;
+    }
+
+    containerBoxesRef.append(...boxes);
+}
+
+function destroyBoxes() {
+    containerBoxesRef.innerHTML = '';
+}
+
+createBtnRef.addEventListener('click', () => createBoxes(numberRef.value));
+
+destroyBtnRef.addEventListener('click', destroyBoxes);
