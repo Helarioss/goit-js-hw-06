989af560101104568cd6f45170bf189cd51bae13
Helarios <kuzinaksenya32@gmail.com>
2021-12-23 20:08:51 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const btnRef = document.querySelector('button.change-color');
+const spanColorRef = document.querySelector('span.color');
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+btnRef.addEventListener('click', () => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor = color;
+    spanColorRef.textContent = color;
+});
