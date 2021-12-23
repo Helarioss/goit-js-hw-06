44c8bd37799e328aa64672483bb5ba675c3ecc32
Helarios <kuzinaksenya32@gmail.com>
2021-12-23 15:06:56 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatoes',
+    'Herbs',
+    'Condiments',
 ];
+
+const ingredientsListRef = document.querySelector('#ingredients');
+
+function makeIngredient(name) {
+    const ingredient = document.createElement('li');
+    ingredient.classList.add('item');
+    ingredient.textContent = name;
+
+    return ingredient;
+}
+
+const ingredientsItemsRefs = ingredients.map(makeIngredient);
+ingredientsListRef.append(...ingredientsItemsRefs);
