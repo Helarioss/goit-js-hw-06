b7aa99f2fa067b002590047b698bd9b86d0907e9
Helarios <kuzinaksenya32@gmail.com>
2021-12-23 18:24:47 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const inputRef = document.querySelector('#name-input');
+const outputRef = document.querySelector('#name-output');
+
+inputRef.addEventListener('input', () => {
+    if (inputRef.value !== '') {
+        outputRef.textContent = inputRef.value;
+    } else outputRef.textContent = 'Anonymous';
+});
